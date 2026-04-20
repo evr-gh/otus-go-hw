84ac891c9d020b500d92aa917b4e06b77d27cf22
Elena.Raspopova <evr.pm@yandex.ru>
2026-04-20 15:53:03 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,67 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"slices"
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	re := regexp.MustCompile("^[\"«']*[^\"'.,!?;:]*[.,!?;:\"»']+$")
+	sre := regexp.MustCompile("[^\"«»'.,!?;:]+")
+	sre.Longest()
+
+	re2 := regexp.MustCompile("^.*[-]+$")
+	sre2 := regexp.MustCompile("[^\"«»'.,!?;:]*[^\"»'.,!?;:-]+")
+	sre2.Longest()
+
+	var res []string
+
+	m := make(map[string]int)
+
+	rvals := []struct {
+		num int
+		val string
+	}{}
+
+	for part := range strings.FieldsSeq(text) {
+		part = strings.ToLower(part)
+
+		switch {
+		case re.MatchString(part):
+			part = sre.FindString(part)
+			if part != "" {
+				m[part]++
+			}
+		case re2.MatchString(part):
+			part = sre2.FindString(part)
+			if part != "" {
+				m[part]++
+			}
+		default:
+			m[part]++
+		}
+	}
+
+	for key, num := range m {
+		rvals = append(rvals,
+			struct {
+				num int
+				val string
+			}{num: num, val: key})
+
+		sort.Slice(rvals, func(i, j int) bool {
+			return (rvals[i].num > rvals[j].num) || ((rvals[i].num == rvals[j].num) && (rvals[i].val < rvals[j].val))
+		})
+		if len(rvals) > 10 {
+			rvals = slices.Delete(rvals, 10, 11)
+		}
+	}
+
+	for i := 0; i < len(rvals); i++ {
+		res = append(res, rvals[i].val)
+	}
+
+	return res
 }

462d6d59a724ca830a65d9f9724ec70090dfc2a9
rev-gh <evr.pm@atomicmail.io>
2026-09-10 22:03:05 +0300

    fix lint errors

--- hw12_13_14_15_16_calendar/internal/server/rpc/server_test.go
+++ hw12_13_14_15_16_calendar/internal/server/rpc/server_test.go
@@ -193,10 +193,14 @@ func TestInterceptorLogging(t *testing.T) {
 
 	fmt.Println(outputted)
 	require.True(t, strings.Contains(outputted, "[INFO] Запуск gRPC сервера: address=\"localhost:5002\""))
-	require.True(t, strings.Contains(outputted, "[INFO] Выполнение метода: method=\"/calendar.Application/CreateEvent\" out={id:1  title:\"Title 1\""))
-	require.True(t, strings.Contains(outputted, "[INFO] Выполнение метода: method=\"/calendar.Application/CreateEvent\" out={id:2  title:\"Title 2\""))
-	require.True(t, strings.Contains(outputted, "[INFO] Начало gRPC потока: method=\"/calendar.Application/ListEvents\" client_stream=false server_stream=true"))
-	require.True(t, strings.Contains(outputted, "[INFO] Конец gRPC потока: method=\"/calendar.Application/ListEvents\" code=OK"))
+	require.True(t, strings.Contains(outputted, "[INFO] Выполнение метода: method=\"/calendar.Application/CreateEvent\""+
+		" out={id:1  title:\"Title 1\""))
+	require.True(t, strings.Contains(outputted, "[INFO] Выполнение метода: method=\"/calendar.Application/CreateEvent\""+
+		" out={id:2  title:\"Title 2\""))
+	require.True(t, strings.Contains(outputted, "[INFO] Начало gRPC потока: method=\"/calendar.Application/ListEvents\""+
+		" client_stream=false server_stream=true"))
+	require.True(t, strings.Contains(outputted, "[INFO] Конец gRPC потока: method=\"/calendar.Application/ListEvents\""+
+		" code=OK"))
 
 	grpcClient.Close()
 	once.Do(cancel)
